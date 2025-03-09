79ebf17d2a4a80b571bff63c8ca6121de8c74957
Floris-Meijvis <f.p.meijvis@students.uu.nl>
2025-03-09 13:21:11 +0100

    Update script.R
    extra class and extra predictor

--- R_project/script.R
+++ R_project/script.R
@@ -1,54 +1,60 @@
 library(tidyverse)
 library(ggplot2)
 
-#we generate a dataset for 2 classes (0 and 1) with 1 predictors, 
+#we generate a dataset for 3 classes (0, 1, 2) with 1 predictors, 
 #with x_1 contributing positively, assume x_1 falls in [-1,1]
 
-#intercepts are very domineering.
+#intercepts values are very important.
 a_0 = -0.5 #intercept class 1
-a_1 = 1  #regression coef class 1
+a_1 = 1  #regression coef class 1 predictor 1
+a_2 = -0.5  #regression coef class 1 predictor 2
 b_0 = 0 #intercept class 2
-b_1 = 1.5 #regression coef class 2
+b_1 = 1.5 #regression coef class 2 predictor 1
+b_2 = -1 #regression coef class 2 predictor 2
 
 N = 10000 #datapoints
 
 set.seed(1234)
-x = runif(n=N, min=-1, max=1)
-P0 = (1)/(1 + exp(a_0 + a_1 * x) + exp(b_0 + b_1 * x))
-P1 = (exp(a_0 + a_1 * x))/(1 + exp(a_0 + a_1 * x) + exp(b_0 + b_1 * x))
-P2 = (exp(b_0 + b_1 * x))/(1 + exp(a_0 + a_1 * x) + exp(b_0 + b_1 * x))
+x_1 = runif(n=N, min=-1, max=1)
+x_2 = runif(n = N, min = -1, max = 1)
+P0 = (1)/(1 + exp(a_0 + a_1 * x_1 + a_2 * x_2) + exp(b_0 + b_1 * x_1 + b_2 * x_2))
+P1 = (exp(a_0 + a_1 * x_1 + a_2 * x_2))/(1 + exp(a_0 + a_1 * x_1 + a_2 * x_2) + exp(b_0 + b_1 * x_1 + b_2 * x_2))
+P2 = (exp(b_0 + b_1 * x_1 + b_2 * x_2))/(1 + exp(a_0 + a_1 * x_1 + a_2 * x_2) + exp(b_0 + b_1 * x_1 + b_2 * x_2))
 
 y = rep(0,N)
 
 for (i in 1:N) {
 y[i] = sample(c(0,1,2), size = 1, replace = TRUE, prob = c(P0[i], P1[i], P2[i]))  #classes.
 }
-Data = data.frame(x, y)
+Data = data.frame(x_1, x_2, y)
 
 # Function to compute log-likelihood
 Log_Likelihood <- function(params, data) {
-  x = data$x
+  x_1 = data$x_1
+  x_2 = data$x_2
   y = data$y
   a_0 = params[1]
   a_1 = params[2]
-  b_0 = params[3]
-  b_1 = params[4]
+  a_2 = params[3]
+  b_0 = params[4]
+  b_1 = params[5]^2 * params[2] # this ensures both have same sign
+  b_2 = params[6]^2 * params[3]
   
   # Compute probabilities
-  log_denominator = log(1 + exp(a_0 + a_1 * x) + exp(b_0 + b_1 * x))
-  log_p1 = a_0 + a_1 * x - log_denominator
-  log_p2 = b_0 + b_1 * x - log_denominator
+  log_denominator = log(1 + exp(a_0 + a_1 * x_1 + a_2 * x_2) + exp(b_0 + b_1 * x_1 + b_2 * x_2))
+  log_p1 = a_0 + a_1 * x_1 + a_2 * x_2 - log_denominator
+  log_p2 = b_0 + b_1 * x_1 + b_2 * x_2 - log_denominator
   log_p0 = -log_denominator
   
   # Select log-probabilities based on y values
   log_likelihoods = ifelse(y == 0, log_p0, ifelse(y == 1, log_p1, log_p2))
   
-  return(-sum(log_likelihoods))  # Negative for minimization
+  return(-sum(log_likelihoods))  # Negative for maximization
 }
 
-Log_Lik = Log_Likelihood(c(a_0, a_1, b_0, b_1), Data)
+Log_Lik = Log_Likelihood(c(a_0, a_1, a_2, b_0, b_1, b_2), Data)
 # Optimization setup
-initial_params = c(-0.5, 1, 0, 1.5)  # Initial guesses for (a_0, a_1, b_0, b_1)
+initial_params = c(-0.5, 1, -0.5, 0, 1.5, -1)  # Initial guesses for params
 
 opt_result <- optim(
   par = initial_params,            # Initial parameter values
@@ -57,18 +63,18 @@ opt_result <- optim(
   method = "BFGS"                   # Optimization method
 )
 
-# Display results
-print(opt_result$par)  # Optimized parameters
 print(opt_result$value)  # Final log-likelihood value
+#Uses BFGS procedure for MLE estimation https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm
+#but is less accurate than using newton-raphson with the hessian matrix
+
+#predicted parameters
+a_0_pred = opt_result$par[1]
+a_1_pred = opt_result$par[2]
+a_2_pred = opt_result$par[3]
+b_0_pred = opt_result$par[4]
+b_1_pred = opt_result$par[5]^2 * opt_result$par[2]
+b_2_pred = opt_result$par[6]^2 * opt_result$par[3]
 
 
-#optim for MLE? uses BFGS
 
-
-#Use BFGS procedure for MLE estimation https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm
-#but is less accurate than using newton-raphson with the hessian matrix.
-
-#for MLE try to use a_1 = cb_1 with c \geq 0 (or c = delta^2) so  coefs have same sign? 
-
-#Extend programm to 3 or more classes, then apply MLE with constraint of Alpha's having the same sign. 
-#maybe after that expand to the more general condition.
+.
